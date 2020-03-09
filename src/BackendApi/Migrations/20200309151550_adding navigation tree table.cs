48240f16f1fc5dccc549a42c9ee7511b5418e2e7
cherkaoui <cherkaoui.mouad@live.fr>
2020-03-09 22:43:21 +0100

    Add: adding NavigationTree entity to the ef model in our the backend project

---
+++ src/BackendApi/Migrations/20200309151550_adding navigation tree table.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackendApi.Migrations
+{
+    public partial class addingnavigationtreetable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
