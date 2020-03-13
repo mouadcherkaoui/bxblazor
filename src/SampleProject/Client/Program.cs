09966597e58a611ed56c8f34951f045d611ad450
cherkaoui <cherkaoui.mouad@live.fr>
2020-03-13 18:51:04 +0100

    Add: adding shell layout component to support state management experiment using blazor-state and mediatr

--- src/SampleProject/Client/Program.cs
+++ src/SampleProject/Client/Program.cs
@@ -2,7 +2,8 @@
 using BxBlazor.Services;
 using Microsoft.AspNetCore.Blazor.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-
+using MediatR;
+using BlazorState;
 namespace blazorwasm.Client
 {
     public class Program
@@ -11,6 +12,11 @@ namespace blazorwasm.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+            builder.Services.AddMediatR(typeof(App).Assembly);
+            builder.Services.AddBlazorState(o =>
+            {
+                o.UseReduxDevToolsBehavior = true;
+            });
             builder.RootComponents.Add<App>("app");
 
             await builder.Build().RunAsync();
