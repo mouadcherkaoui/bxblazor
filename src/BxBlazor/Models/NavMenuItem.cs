05bc4e6b1ce8d74287e6b1aa04c6cb70e9e9716d
cherkaoui <cherkaoui.mouad@live.fr>
2020-03-20 19:33:37 +0100

    Fix: fixing navigation active item class

--- src/BxBlazor/Models/NavMenuItem.cs
+++ src/BxBlazor/Models/NavMenuItem.cs
@@ -10,8 +10,8 @@ namespace BxBlazor.Models
         public string Uri { get; set; }
         public bool HasIcon { get; set; }
         public string Type { get; set; }
-        public bool IsActive { get; set; }
-        public bool IsExpanded { get; set; }
+        public bool IsActive { get; set; } = false;
+        public bool IsExpanded { get; set; } = false;
         public IEnumerable<NavMenuItem> SubMenuItems { get; set; }
     }
 }
