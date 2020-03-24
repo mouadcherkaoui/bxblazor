7e2b9e36bd67b03b729d33a96d9f9383d9ffd97c
cherkaoui <cherkaoui.mouad@live.fr>
2020-03-24 10:27:35 +0100

    Up: adding search oninput for the BxDatatable

---
+++ src/BxBlazor/Events/SearchEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BxBlazor.Events
+{
+    public class SearchEventArgs : KeyboardEventArgs
+    {
+        public SearchEventArgs(KeyboardEventArgs args, string searchValue)
+        {
+            Key = args.Key;
+            Location = args.Location;
+            MetaKey = args.MetaKey;
+            Repeat = args.Repeat;
+            SearchValue = searchValue;
+        }
+        public string SearchValue { get; set; }
+    }
+}
